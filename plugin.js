25a92d5ea8ac0e4348ea574b7867fca568d2d3ab
albertosouza <alberto.souza.99@gmail.com>
2017-01-15 18:30:29 -0200

    init

---
+++ plugin.js
@@ -0,0 +1,19 @@
+/**
+ * Plugin.js file, set configs, routes, hooks and events here
+ *
+ * see http://wejs.org/docs/we/plugin
+ */
+module.exports = function loadPlugin(projectPath, Plugin) {
+  const plugin = new Plugin(__dirname);
+  // set plugin configs
+  // plugin.setConfigs({
+  // });
+  // set plugin routes
+  // plugin.setRoutes({
+  // });
+  plugin.fastLoader = function fastLoader(we, done) {
+    done();
+  };
+
+  return plugin;
+};

818815bcad26d7359eb0071c60c5c01216c6b658
albertosouza <alberto.souza.99@gmail.com>
2017-02-16 08:24:12 -0200

    add tag search methods in article.create controller

---
+++ app/controllers/articles/create.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import ENV from "../../config/environment";
+
+export default Ember.Controller.extend({
+  ajax: Ember.inject.service(),
+
+  actions: {
+    searchCategoryTerms(term) {
+      let url = `${ENV.API_HOST}/api/v1/term-texts?term=${term}&vocabularyName=Category`;
+      return this.get('ajax')
+      .request(url)
+      .then((json) => json.term );
+    },
+    searchTagsTerms(term) {
+      let url = `${ENV.API_HOST}/api/v1/term-texts?term=${term}&vocabularyName=Tags`;
+      return this.get('ajax')
+      .request(url)
+      .then((json) => json.term );
+    }
+  }
+});

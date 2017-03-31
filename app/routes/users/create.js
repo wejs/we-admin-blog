edf053d30786de4fb35a210aeeddfb762fb5b933
albertosouza <alberto.souza.99@gmail.com>
2017-03-31 12:05:56 -0300

    add the create user action ref #3

--- app/routes/users/create.js
+++ app/routes/users/create.js
@@ -6,7 +6,28 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
   model() {
     return Ember.RSVP.hash({
-      space: {}
+      user: {}
     });
+  },
+  actions: {
+    create(data) {
+      const i18n = this.get('i18n');
+
+      const record = this.store.createRecord('user', data);
+
+      record.active = true;
+
+      record.save()
+      .then( (r)=> {
+        this.get('notifications').success(i18n.t('user.registered.success.msg'));
+
+        this.transitionTo('users.item', r.id);
+        // success
+        return r;
+      })
+      .catch( (err)=> {
+        this.send('queryError', err);
+      });
+    }
   }
 });

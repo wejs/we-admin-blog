f1d699611451fcbe1a549502d882c9f0b11d2221
albertosouza <alberto.souza.99@gmail.com>
2017-04-02 21:49:26 -0300

    improve article editor with more functions

--- app/controllers/articles/item.js
+++ app/controllers/articles/item.js
@@ -12,6 +12,15 @@ export default Ember.Controller.extend({
     get() {
       const opts = {
         min_height: 400,
+        theme: 'modern',
+        plugins: [
+          'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+          'searchreplace wordcount visualblocks visualchars code fullscreen',
+          'insertdatetime media nonbreaking save table contextmenu directionality',
+          'emoticons template paste textcolor colorpicker textpattern imagetools codesample'
+        ],
+        toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
         language: this.getEditorLocale(),
         language_url: this.getEditorLocaleUrl()
       };
